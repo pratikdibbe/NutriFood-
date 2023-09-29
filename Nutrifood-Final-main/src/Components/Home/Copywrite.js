808a119986453a3d5dc17447fd48fd44f2bc8a09
Pratik Dibbe <116033040+pratikdibbe@users.noreply.github.com>
2023-09-29 13:23:39 +0530

    Add files via upload

---
+++ Nutrifood-Final-main/src/Components/Home/Copywrite.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import '../CSS/Copywrite.css'
+
+function Copywrite() {
+
+  return (
+    <div className="Copywrite">
+      <p>All rights reserved @NUTRI-FOOD</p>
+   
+    </div>
+  );
+}
+
+export default Copywrite;
